3155fc09d396ef44be11a384dbc0a9e3008f8f80
LaserToothLiger <lasertoothliger@gmail.com>
2025-08-06 16:23:44 -0700

    v1.0.11
    To be less useless, the parenthetical value in HIPAA caster power level labels denotes number of psycasts known, not total level of psycasts known.

--- 1.6/Source/HVPAA/Class1.cs
+++ 1.6/Source/HVPAA/Class1.cs
@@ -1097,10 +1097,25 @@ namespace HVPAA
             {
                 if (HVPAA_Mod.settings.showNPCasterLevel && this.link1.Target != null && this.link1.Target.Thing != null && this.link1.Target.Thing is Pawn p && !p.IsColonistPlayerControlled)
                 {
-                    return "HVPAA_NPC_mote".Translate(p.GetPsylinkLevel(), HautsUtility.TotalPsycastLevel(p));
+                    return "HVPAA_NPC_mote".Translate(p.GetPsylinkLevel(), this.TotalPsycasts(p));
                 }
                 return " ";
             }
+        }
+        public int TotalPsycasts(Pawn p)
+        {
+            int totalPsycastPower = 0;
+            if (p.abilities != null)
+            {
+                for (int i = 0; i < p.abilities.abilities.Count; i++)
+                {
+                    if (p.abilities.abilities[i].def.IsPsycast)
+                    {
+                        totalPsycastPower++;
+                    }
+                }
+            }
+            return totalPsycastPower;
         }
     }
     //DefModExtensions
