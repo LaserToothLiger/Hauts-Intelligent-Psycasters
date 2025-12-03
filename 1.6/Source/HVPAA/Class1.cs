c4df688b861ae299cd7d778a192a6996364ffa52
LaserToothLiger <lasertoothliger@gmail.com>
2025-12-02 23:19:14 -0800

    v1.0.24
    New spec caster type that gets used in HEMP.

--- 1.6/Source/HVPAA/Class1.cs
+++ 1.6/Source/HVPAA/Class1.cs
@@ -5376,7 +5376,7 @@ namespace HVPAA
             yield break;
         }
     }
-    //spec caster incidents
+    //spec caster stuff
     public class StrikeSquaddie : DefModExtension
     {
         public StrikeSquaddie()
@@ -5412,6 +5412,76 @@ namespace HVPAA
         {
             return 1;
         }
+    }
+    public class HediffCompProperties_Heresiarch : HediffCompProperties
+    {
+        public HediffCompProperties_Heresiarch()
+        {
+            this.compClass = typeof(HediffComp_Heresiarch);
+        }
+        public int deadlifePeriodicity;
+        public DamageDef deadlifeDamage;
+        public float deadlifeRadius;
+        public FactionDef reinforcingFaction;
+        public IntRange reinforcementCount;
+    }
+    public class HediffComp_Heresiarch : HediffComp
+    {
+        public HediffCompProperties_Heresiarch Props
+        {
+            get
+            {
+                return (HediffCompProperties_Heresiarch)this.props;
+            }
+        }
+        public override void CompPostTickInterval(ref float severityAdjustment, int delta)
+        {
+            base.CompPostTickInterval(ref severityAdjustment, delta);
+            if (this.Pawn.IsHashIntervalTick(this.Props.deadlifePeriodicity, delta))
+            {
+                if (!this.calledReinforcementsYet && this.Pawn.Map != null && this.Pawn.Map.Tile.Valid)
+                {
+                    this.calledReinforcementsYet = true;
+                    Faction f = this.Pawn.Faction ?? Find.FactionManager.FirstFactionOfDef(this.Props.reinforcingFaction);
+                    Lord lord = this.Pawn.lord ?? LordMaker.MakeNewLord(f, new LordJob_AssaultColony(f, false, false, false, false, false, false, false), this.Pawn.Map, null);
+                    int toSpawn = this.Props.reinforcementCount.RandomInRange;
+                    while (toSpawn > 0)
+                    {
+                        toSpawn--;
+                        Pawn reinforcement = PawnGenerator.GeneratePawn(f.def.pawnGroupMakers.Where((PawnGroupMaker pgm)=>pgm.kindDef == PawnGroupKindDefOf.Combat).RandomElement().options.RandomElement().kind, f, null);
+                        GenSpawn.Spawn(reinforcement, CellFinder.RandomClosewalkCellNear(this.Pawn.Position, this.Pawn.Map, 10), this.Pawn.Map, WipeMode.Vanish);
+                        FleckMaker.Static(reinforcement.Position, this.Pawn.Map, FleckDefOf.PsycastSkipInnerExit, 1f);
+                        FleckMaker.Static(reinforcement.Position, this.Pawn.Map, FleckDefOf.PsycastSkipOuterRingExit, 1f);
+                        if (!reinforcement.Downed)
+                        {
+                            Lord lord2 = reinforcement.lord;
+                            if (lord2 != null)
+                            {
+                                lord2.RemovePawn(reinforcement);
+                            }
+                            lord.AddPawn(reinforcement);
+                        }
+                    }
+                    if (this.Pawn.lord == null)
+                    {
+                        lord.AddPawn(this.Pawn);
+                    } else if (this.Pawn.lord != lord) {
+                        this.Pawn.lord.RemovePawn(this.Pawn);
+                        lord.AddPawn(this.Pawn);
+                    }
+                }
+                if (this.Pawn.Spawned && ModsConfig.AnomalyActive)
+                {
+                    GenExplosion.DoExplosion(this.Pawn.Position,this.Pawn.Map,this.Props.deadlifeRadius,this.Props.deadlifeDamage,this.Pawn, postExplosionGasType:GasType.DeadlifeDust);
+                }
+            }
+        }
+        public override void CompExposeData()
+        {
+            base.CompExposeData();
+            Scribe_Values.Look<bool>(ref this.calledReinforcementsYet, "calledReinforcementsYet", false, false);
+        }
+        public bool calledReinforcementsYet;
     }
     //utilities
     public class HVPAAUtility
